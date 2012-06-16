33ff8b332abc7b59b913d815c9eca979fea10a35
Nick Dimiduk <nick@climate.com>
2012-06-15 21:48:02 -0700

    updates for me
    - add jruby and puppet modes to alist.
    - add TCC root for sundry scripts from within eshell

--- ndimiduk.el
+++ ndimiduk.el
@@ -9,12 +9,20 @@
      (setq org-src-fontify-natively t)
      (setq org-src-tab-acts-natively t)))
 
-;; Steve Yegge's js2-mode is full of hotness, but is it maintained any
-;; longer..?
+;; Steve Yegge's js2-mode is full of hotness, but is it maintained any longer..?
 ;; http://steve-yegge.blogspot.com/2008/03/js2-mode-new-javascript-mode-for-emacs.html
 (ensure-packages '(js2-mode))
 
+;; jrb files for scripting the HBase shell
+(push '("\\.jrb$" . ruby-mode) auto-mode-alist)
 
 ;; pull in thrift-mode. it would be great to move this to marmalade...
 (require 'thrift-mode)
-(add-to-list 'auto-mode-alist '("\\.thrift$" . thrift-mode))
+(push '("\\.thrift$" . thrift-mode) auto-mode-alist)
+
+;; puppet!
+(ensure-packages '(puppet-mode))
+(push '("\\.pp$" . puppet-mode) auto-mode-alist)
+
+;; WB_ROOT for tcc apps
+(setenv "WB_ROOT" (concat (getenv "HOME") "/repos"))
