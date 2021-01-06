a834dd35398a4d538e002376e7639fab3e9fa2bf
Nick Dimiduk <ndimiduk@gmail.com>
2021-01-06 11:00:38 -0800

    initial config using straight.el
    * basic UI customizations
    * install magit
    * install sanityinc-solarized themes
    * .gitignore based on https://github.com/github/gitignore/blob/master/Global/Emacs.gitignore

---
+++ init.el
@@ -0,0 +1,48 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (sanityinc-solarized-light)))
+ '(custom-safe-themes
+   (quote
+    ("4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" default)))
+ '(global-hl-line-mode t)
+ '(inhibit-startup-screen t)
+ '(menu-bar-mode nil)
+ '(scroll-bar-mode nil)
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; bootstrap straight.el
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(straight-use-package 'use-package)
+(use-package magit
+  :straight t
+  :init (setq vc-handled-backends nil)
+  :bind (("C-c g" . 'magit-status)
+         ("C-x g" . 'magit-status)))
+(use-package color-theme-sanityinc-solarized
+  :straight t
+  :init (if (null window-system)
+	    (color-theme-sanityinc-solarized-light)
+	  (color-theme-sanityinc-solarized-dark)))
+
+
