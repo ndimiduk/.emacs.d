a11bc693a9b72b8762ec11ca53f7c7b11c57fc5a
Nick Dimiduk <ndimiduk@gmail.com>
2021-01-06 11:28:08 -0800

    tweak a few additional customize settings

--- init.el
+++ init.el
@@ -3,14 +3,17 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (sanityinc-solarized-light)))
+ '(custom-enabled-themes '(sanityinc-solarized-light))
  '(custom-safe-themes
-   (quote
-    ("4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" default)))
+   '("4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" default))
+ '(fill-column 98)
  '(global-hl-line-mode t)
  '(inhibit-startup-screen t)
+ '(make-backup-files nil)
  '(menu-bar-mode nil)
  '(scroll-bar-mode nil)
+ '(straight-use-package-by-default t)
+ '(straight-vc-git-default-clone-depth 1)
  '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
