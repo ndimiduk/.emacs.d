b4770fe68710e0fd428d3c16d01f6cd5ea3a0fc7
Nick Dimiduk <ndimiduk@gmail.com>
2021-01-08 12:07:15 -0800

    add selectrum with prescient. fix warning re: theme not yet loaded

--- init.el
+++ init.el
@@ -3,7 +3,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(sanityinc-solarized-light))
  '(custom-safe-themes
    '("4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" default))
  '(fill-column 98)
@@ -47,5 +46,12 @@
   :init (if (null window-system)
 	    (color-theme-sanityinc-solarized-light)
 	  (color-theme-sanityinc-solarized-dark)))
-
+(use-package selectrum
+  :straight t
+  :init (selectrum-mode +1))
+(use-package selectrum-prescient
+  :straight t
+  :init (progn
+	  (selectrum-prescient-mode +1)
+	  (prescient-persist-mode +1)))
 
