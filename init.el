baca98d9c266a38e308a84073d2172f435327478
Nick Dimiduk <ndimiduk@gmail.com>
2021-01-13 15:31:27 -0800

    further tweaks

--- init.el
+++ init.el
@@ -3,10 +3,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
+ '(custom-enabled-themes '(sanityinc-solarized-light))
  '(custom-safe-themes
    '("4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" default))
  '(fill-column 98)
- '(global-hl-line-mode t)
  '(inhibit-startup-screen t)
  '(make-backup-files nil)
  '(menu-bar-mode nil)
@@ -34,24 +35,36 @@
       (goto-char (point-max))
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
-
 (straight-use-package 'use-package)
+
+(use-package hl-line
+  :init (global-hl-line-mode t))
+
 (use-package magit
-  :straight t
   :init (setq vc-handled-backends nil)
   :bind (("C-c g" . 'magit-status)
          ("C-x g" . 'magit-status)))
+
 (use-package color-theme-sanityinc-solarized
-  :straight t
   :init (if (null window-system)
 	    (color-theme-sanityinc-solarized-light)
 	  (color-theme-sanityinc-solarized-dark)))
+
 (use-package selectrum
-  :straight t
   :init (selectrum-mode +1))
 (use-package selectrum-prescient
-  :straight t
   :init (progn
 	  (selectrum-prescient-mode +1)
 	  (prescient-persist-mode +1)))
 
+(use-package company
+  :init (add-hook 'after-init-hook 'global-company-mode)
+  :bind (("M-/" . 'company-complete)))
+
+(use-package exec-path-from-shell
+  :if (memq window-system '(mac ns))
+  :config (exec-path-from-shell-initialize))
+(use-package flycheck
+  :init (global-flycheck-mode))
+
+(use-package puppet-mode)
