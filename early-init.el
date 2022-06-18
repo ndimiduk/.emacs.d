85e4161d93c9316152166e1fd0a0d2b3e96ee0f5
Nick Dimiduk <ndimiduk@apache.org>
2022-06-18 12:56:40 +0200

    upgrade emacs and straight

---
+++ early-init.el
@@ -0,0 +1,5 @@
+;; From https://github.com/raxod502/straight.el#getting-started
+;;
+;; Users of Emacs versions >= 27 will want to add:
+(setq package-enable-at-startup nil)
+;; to their early init-file to prevent package.el loading packages prior to their init-file loading.
