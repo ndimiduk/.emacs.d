6613cc20d23b3e87b489cb34842f5a2f028d3d38
Nick Dimiduk <ndimiduk@gmail.com>
2023-03-20 21:38:08 +0100

    setup rustic

--- init.el
+++ init.el
@@ -73,9 +73,50 @@
 	  (selectrum-prescient-mode +1)
 	  (prescient-persist-mode +1)))
 
+(use-package yasnippet
+  :config
+  (yas-reload-all)
+  (add-hook 'prog-mode-hook 'yas-minor-mode)
+  (add-hook 'text-mode-hook 'yas-minor-mode))
+
+(defun company-yasnippet-or-completion ()
+  (interactive)
+  (or (do-yas-command)
+      (company-complete-common)))
+
+(defun check-expansion ()
+  (save-excursion
+    (if (looking-at "\\_>") t
+      (backward-char 1)
+      (if (looking-at "\\.") t
+        (backward-char 1)
+        (if (looking-at "::") t nil)))))
+
+(defun do-yas-expand ()
+  (let ((yas/fallback-behavior 'return-nil))
+    (yas/expand)))
+
+(defun tab-indent-or-complete ()
+  (interactive)
+  (if (minibufferp)
+      (minibuffer-complete)
+    (if (or (not yas/minor-mode)
+            (null (do-yas-expand)))
+        (if (check-expansion)
+            (company-complete-common)
+          (indent-for-tab-command)))))
+
 (use-package company
   :init (add-hook 'after-init-hook 'global-company-mode)
-  :bind (("M-/" . 'company-complete)))
+  :bind
+  (:map company-active-map
+        ("C-n" . company-select-next)
+        ("C-p" . company-select-previous)
+        ("M-<" . company-select-first)
+        ("M->" . company-select-last)
+        ("M-/" . company-complete))
+  (:map company-mode-map
+        ("[?\t]" . tab-indent-or-complete)))
 
 (use-package flycheck
   :init (global-flycheck-mode))
@@ -92,3 +133,32 @@
 ;(use-package bash-completion
 ;  :config (add-hook 'shell-dynamic-complete-functions
 ;                    'bash-completion-dynamic-complete))
+
+;;; Configure rustic with rust-analyzer
+;(use-package f) ; missing dependency declaration for rustic
+(use-package rustic
+  :bind (:map rustic-mode-map
+              ("M-j" . lsp-ui-imenu)
+              ("M-?" . lsp-find-references)
+              ("C-c C-c l" . flycheck-list-errors)
+              ("C-c C-c a" . lsp-execute-code-action)
+              ("C-c C-c r" . lsp-rename)
+              ("C-c C-c q" . lsp-workspace-restart)
+              ("C-c C-c Q" . lsp-workspace-shutdown)
+              ("C-c C-c s" . lsp-rust-analyzer-status))
+  :config
+  (setq rustic-analyzer-command '("rustup" "run" "stable" "rust-analyzer"))
+  (setq rustic-format-on-save t))
+
+(use-package lsp-mode
+  :commands lsp
+  :config
+  (setq lsp-enable-snippet nil)
+  (add-hook 'lsp-mode-hook 'lsp-ui-mode))
+
+(use-package lsp-ui
+  :commands lsp-ui-mode
+  :custom
+  (lsp-ui-peek-always-show t)
+  (lsp-ui-sideline-show-hover t)
+  (lsp-ui-doc-enable nil))
