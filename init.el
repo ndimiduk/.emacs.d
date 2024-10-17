9f9e4ae63206dee30b8bc4b89a84c623d676f91b
Nick Dimiduk <ndimiduk@gmail.com>
2024-10-17 09:26:02 +0200

    don't safe prompted passwords

--- init.el
+++ init.el
@@ -8,6 +8,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(auth-source-save-behavior nil)
  '(column-number-mode t)
  '(fill-column 98)
  '(indent-tabs-mode nil)
