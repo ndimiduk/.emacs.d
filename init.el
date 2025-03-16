2a1b335143f4b0faa5be61c29de4470bd0ac78d0
Nick Dimiduk <ndimiduk@gmail.com>
2025-03-16 11:21:43 +0100

    bump solarized theme shas

--- init.el
+++ init.el
@@ -115,10 +115,15 @@
   :custom
   (custom-safe-themes
    '(;; sanityinc-solarized-dark
-     "4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328"
+     "48d34b6afe72407ca494387c8bea495bb2deee96bd88516f302db1f11e1810a1"
      ;; sanityinc-solarized-light
-     "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4"
+     "6819104c5f7d70485b32c10323aa396806d282fcee5b707e462bf3d156f44c39"
      default))
+  (custom-enabled-themes
+   '(
+     sanityinc-solarized-light
+     sanityinc-solarized-dark
+     ))
   :config
   (load-theme 'sanityinc-solarized-light t))
 
